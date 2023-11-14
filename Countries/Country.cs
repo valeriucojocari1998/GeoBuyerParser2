d7dd5a1ae3c872ec0206ad1894b1df872e97072d
valeriucoj98 <valeriucojocari1998@gmail.com>
2023-11-14 12:41:38 +0200

    minor fix

---
+++ Countries/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GeoBuyerParser2.Countries;
+
+public static class Country
+{
+    public static IDictionary<string, string> Config = new Dictionary<string, string>()
+    {
+        {"SK", "[out:json];area['ISO3166-1'='SK']->.boundary;" +
+                        "(node(area.boundary)['shop'='yes'];" +
+                        "way(area.boundary)['shop'='yes'];" +
+                        "relation(area.boundary)['shop'='yes'];);out;" }
+    };
+}
+
--- Services/OSMService.cs
+++ Services/OSMService.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿#nullable enable
+using System.Text;
+using GeoBuyerParser2.Countries;
 using GeoBuyerParser2.Models;
 using GeoBuyerParser2.Repositories;
 using Newtonsoft.Json;
@@ -15,15 +17,25 @@ public class OSMService
         _repository = repository;
     }
 
-    public async Task<List<Location>> GetData()
+    public async Task<List<Location>> GetData(string country)
+    {
+        var query = Country.Config.TryGetValue(country, out var info) ? info : null;
+        if (query == null)
+        {
+            throw new Exception("wrong country name");
+        }
+            var json = await GetDataFromOverpass(query);
+            var parsedData = ParseJsonData(json);
+            _repository.InsertLocations(parsedData);
+            var data = _repository.GetLocations();
+            return parsedData;
+    }
+
+    private async Task<string> GetDataFromOverpass(string query)
     {
         try
         {
             var overpassEndpoint = "https://overpass-api.de/api/interpreter";
-            var query = "[out:json];area['ISO3166-1'='SK']->.boundary;" +
-                        "(node(area.boundary)['shop'='yes'];" +
-                        "way(area.boundary)['shop'='yes'];" +
-                        "relation(area.boundary)['shop'='yes'];);out;";
 
             using (var httpClient = new HttpClient())
             {
@@ -32,17 +44,12 @@ public class OSMService
                 var response = await httpClient.PostAsync(overpassEndpoint, content);
                 response.EnsureSuccessStatusCode();
 
-                var json = await response.Content.ReadAsStringAsync();
-                var parsedData = ParseJsonData(json);
-                _repository.InsertLocations(parsedData);
-                var data = _repository.GetLocations();
-                return parsedData;
+                return await response.Content.ReadAsStringAsync();
             }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error making the API request: {ex.Message}");
-            return new List<Location>();
+            throw new Exception("error at overpass api request", ex);
         }
     }
 
@@ -59,45 +66,12 @@ public class OSMService
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error parsing JSON data: {ex.Message}");
-            return new List<Location>();
+            throw new Exception("error at parsing the json data", ex);
         }
     }
 }
 
 public class OsmResponse
 {
-    public List<OsmElement> elements { get; set; }
-}
-
-public class OsmElement
-{
-    public string type { get; set; }
-    public long id { get; set; }
-    public double lat { get; set; }
-    public double lon { get; set; }
-    public Dictionary<string, string> tags { get; set; }
-    public string addr_city { get; set; }
-    public string addr_housenumber { get; set; }
-    public string addr_postcode { get; set; }
-    public string addr_street { get; set; }
-    public string amenity { get; set; }
-    public string brand { get; set; }
-    public string brand_wikidata { get; set; }
-    public string brand_wikipedia { get; set; }
-    public string email { get; set; }
-    public string fuel_adblue { get; set; }
-    public string fuel_diesel { get; set; }
-    public string fuel_diesel_plus { get; set; }
-    public string fuel_lpg { get; set; }
-    public string fuel_octane_100 { get; set; }
-    public string fuel_octane_95 { get; set; }
-    public string import_ref { get; set; }
-    public string name { get; set; }
-    public string opening_hours { get; set; }
-    public string website { get; set; }
-    public string note { get; set; }
-    public string compressed_air { get; set; }
-    public string wheelchair { get; set; }
-        // Add more properties as needed for other fields in the JSON
+    public List<OsmElement> elements { get; set; } = new List<OsmElement>();
 }
